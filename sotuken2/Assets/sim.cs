5e09e8739a6f57353011ead33ea9a251f588702f
tealate <96100936+tealate@users.noreply.github.com>
2023-01-03 22:19:51 +0900

    a

---
+++ sotuken2/Assets/sim.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sim : MonoBehaviour
+{
+    public Vector3 tensor;
+    public Vector3 contpnt; //接触位置
+    public Vector3 masp;  //重心
+    public Vector3 vecf; //接触位置のベクトル
+    public float force;  //接触位置の力
+    Rigidbody rb;
+    public GameObject obj;
+    GameObject ob;
+    public int a = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //GameObject obj = (GameObject)Resources.Load("colid");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(a);
+        a = 0;
+        tensor = rb.inertiaTensor;
+        masp = rb.centerOfMass;
+    }
+    void OnCollisionStay(Collision collision)
+    {
+        vecf = collision.impulse;
+        contpnt = collision.contacts[0].point;
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            // Visualize the contact point
+            //Debug.DrawRay(contact.point, contact.normal, Color.black);
+            //Debug.Log(contact.otherCollider.name);
+            a++;
+        }
+        force = collision.impulse.magnitude / Time.deltaTime;
+        //Debug.DrawRay(transform.TransformPoint(masp), collision.impulse* 3, Color.red,0.0f,false);
+    }
+}
--- sotuken2/Assets/simu.cs
+++ sotuken2/Assets/simu.cs
@@ -4,45 +4,46 @@ using UnityEngine;
 
 public class simu : MonoBehaviour
 {
+    public Vector3 testforce; //テスト用に自由に力を加える、加える位置は中心
     public Vector3 tensor;
     public Vector3 contpnt; //接触位置
     public Vector3 masp;  //重心
     public Vector3 vecf; //接触位置のベクトル
     public float force;  //接触位置の力
-    Rigidbody rb;
-    public GameObject obj;
-    GameObject ob;
-    public int a=0;
+    public Vector3 imp;
+    public float impul;
+    Rigidbody rb; //オブジェクト自身
+    public Vector3 returnforce; //接触に対し与える力
+    public Vector3 returnbreak; //接触に対し与える摩擦
+    public int cont=0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //GameObject obj = (GameObject)Resources.Load("colid");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(a);
-        a = 0;
+        //Debug.Log(cont);
+        cont = 0;
         tensor = rb.inertiaTensor;
         masp = rb.centerOfMass;
     }
     void OnCollisionStay(Collision collision)
     {
+        Debug.Log("a");
         vecf = collision.impulse;
         contpnt = collision.contacts[0].point;
         foreach (ContactPoint contact in collision.contacts)
         {
             // Visualize the contact point
             Debug.DrawRay(contact.point, contact.normal, Color.black);
-            Debug.Log(contact.otherCollider.name);
-            a++;
+            //Debug.Log(contact.otherCollider.name);
+            cont++;
         }
-        //for(int i = 0;collision.contacts[i].point != null&& i < 100; i++)
-        //Instantiate(obj, collision.contacts[i].point, Quaternion.identity);
         force = collision.impulse.magnitude/Time.deltaTime;
-            Debug.DrawRay(transform.TransformPoint(masp), collision.impulse* 3, Color.red,0.0f,false);
+            //Debug.DrawRay(transform.TransformPoint(masp), collision.impulse* 3, Color.red,0.0f,false);
     }
 }
